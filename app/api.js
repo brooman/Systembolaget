82301158339917b68152953bed792496ea2f940f
laykith <laykith@gmail.com>
2019-02-15 13:10:41 +0100

    Merged connection into Api

--- app/api.js
+++ app/api.js
@@ -1,9 +1,22 @@
-const database = require('../database/connection.js')
+'use strict'
+
+const mongoose = require('mongoose')
+
+//Models
+const Product = require('../database/models/Product')
+const ProductHelper = require('../database/models/helpers/ProductHelper')
+
 const parser = require('fast-xml-parser')
 const fetch = require('node-fetch')
 
+//Connect to DB
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true
+})
+
+
 module.exports = {
-  refresh: async () => {
+  updateData: async () => {
     const fetchJSON = () => {
       return fetch('https://www.systembolaget.se/api/assortment/products/xml')
         .then(response => response.text())
@@ -12,14 +25,33 @@ module.exports = {
             localeRange: 'åäö',
           })
         })
-        .catch(err => console.error(err));
+        .catch(err => console.error(err))
     }
 
     const data = await fetchJSON()
 
     data.artiklar.artikel.forEach(item => {
 
-      database.updateOrCreate(item);
+      const query = {
+        ProductNumber: item.nr
+      }
+
+      const config = {
+        upsert: true,
+        useFindAndModify: false
+      }
+
+      const data = ProductHelper.builder(item)
+
+      Product.findOneAndUpdate(query, data, config, (err) => {
+        if (err) {
+          console.log(err)
+        }
+      })
     })
+  },
+  fetchData: (params) => {
+    Product.find(params)
+    return data
   }
 }
